820fd0a7d971b8167646cf5716f52ee6e5bce624
Дмитрий Красилов <krasilov.d-post@yandex.ru>
2022-02-15 18:58:39 +0800

    Doesn't work

---
+++ Search.py
@@ -0,0 +1,22 @@
+class Training:
+
+        def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+
+
+
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+def read_package(workout_type: str, data: list) -> Training:
+    """Прочитать данные полученные от датчиков."""
+    pass
